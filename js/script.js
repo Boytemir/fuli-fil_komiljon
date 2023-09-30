da07aae34f8bdf4dd56716232dbf588ec8b1c2ed
Boytemir <boytemirxayitmurodov@gmail.com>
2023-09-30 12:38:07 +0500

    add sign up

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
 // Swiper js
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 4,
-//     spaceBetween: 20,
-//     freeMode: true,
-//     autoplay: {
-//         delay: 2500,
-//         disableOnInteraction: false,
-//     },
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     }
-// });
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
@@ -38,4 +25,31 @@ var swiper = new Swiper(".mySwiper", {
         spaceBetween: 20,
       },
     },
-  });
+});
+
+
+// sign up
+let kurs__btn = document.querySelectorAll(".kurs__btn");
+let sign_up = document.querySelector(".sign-up");
+let sign__up__box = document.querySelector(".sign-up__box");
+
+kurs__btn.forEach(element => {
+    element.addEventListener("click", () => {
+        sign_up.classList.add("active");
+    })
+});
+
+sign_up.addEventListener("click", (e) => {
+    // e.stopImmediatePropagation();
+    sign_up.classList.remove("active");
+});
+
+sign__up__box.addEventListener("click", (e) => {
+    e.stopImmediatePropagation();
+});
+
+let sign__up__btn = document.querySelector(".sign-up__btn");
+
+sign__up__btn.addEventListener("click", (e) => {
+    e.preventDefault();
+});
